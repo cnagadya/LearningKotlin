27f47743359b9800e79cf4d022ced74f13a457d1
Christine R. Nagadya <ncritah@gmail.com>
2019-07-03 19:38:21 +0200

    ranges

--- Basics/src/helloWorld.kt
+++ Basics/src/helloWorld.kt
@@ -1,6 +1,6 @@
 import samplePackage.Person
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     var a: Int
     var num = 10
 
@@ -12,9 +12,33 @@ fun main(args: Array<String>){
     println(9 * 2.4)
 
 
+
     var newPerson = Person("Ugandan")
     newPerson.displayName("Christine")
     newPerson.age = 100
     newPerson.displayAge(newPerson.age) //simply using age wont work
     newPerson.displayNationality()
+
+    /*
+    * ===Ranges
+    * */
+
+    // ascending
+    val ascending = 1..5
+    println(ascending)
+
+    //descending
+    val descending = 5 downTo 1
+
+    // excluding values in range
+    val select = 1..10 step 2
+    println(1 in select)
+    println(2 in select)
+
+    // for string characters
+    val charStr = "a".."z"
+    println("a" in charStr)
+
+    // for characters
+    val chars = 'a'..'z'
 }
