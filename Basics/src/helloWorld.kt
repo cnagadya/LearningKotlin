dc9f8df80924e8d2ff1816bd80698a32e56fbd6b
Christine R. Nagadya <ncritah@gmail.com>
2019-07-04 14:03:50 +0200

    When statement

--- Basics/src/helloWorld.kt
+++ Basics/src/helloWorld.kt
@@ -1,55 +1,21 @@
-import samplePackage.Person
+fun main() {
 
-fun main(args: Array<String>) {
-    var a: Int
-    var num = 10
-
-    val constant = "I can not change"
-
-//    constant = "Try me" ===will throw an error
-    a = 4
-    println("Hello World ${num + a}  $constant")
-    println(9 * 2.4)
-
-
-
-    var newPerson = Person("Ugandan")
-    newPerson.displayName("Christine")
-    newPerson.age = 100
-    newPerson.displayAge(newPerson.age) //simply using age wont work
-    newPerson.displayNationality()
-
-    /*
-    * ===Ranges
-    * */
-
-    // ascending
-    val ascending = 1..5
-    println(ascending)
-
-    //descending
-    val descending = 5 downTo 1
-
-    // excluding values in range
-    val select = 1..10 step 2
-    println(1 in select)
-    println(2 in select)
-
-    // for string characters
-    val charStr = "a".."z"
-    println("a" in charStr)
-
-    // for characters
-    val chars = 'a'..'z'
-
-    /*
-    *
-     * If expression
+    /**
+     * When Statement
      */
-    var max = if (num > a)
-        a
-    else num
 
-    println("Max = $max")
+    val x = 4 // test with 1 and any other number
+
+    when (x) {
+        0, 1 -> println("x is $x") // check 2 values
+        2 -> println("x is 2")
+
+        in 3..7 -> println("x is $x which is between 3 and 7, inclusive")
+
+        // use else for default statement
+        else -> println("unexpected value for x")
+    }
+
+    //ps can also be used as an expression just like the if
 
 }
