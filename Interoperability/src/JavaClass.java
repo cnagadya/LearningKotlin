3a9a8b3663dccb27489928c69ce79517631706cf
Christine R. Nagadya <ncritah@gmail.com>
2019-07-05 12:34:21 +0200

    Interoperability - accessing kotlin inside a Java class

---
+++ Interoperability/src/JavaClass.java
@@ -0,0 +1,6 @@
+public class JavaClass {
+    public static void main(String[] args) {
+        InteroperabilityKt.printer("Christine");
+
+    }
+}
---
+++ Interoperability/src/interoperability.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+}
+
+fun printer(name: String){
+    println("Hello $name, I am from Kotlin")
+
+}
